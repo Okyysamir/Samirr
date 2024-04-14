793524178bf6dc6df64ae5fec7680518ce8a7516
Okyysamir <163244202+Okyysamir@users.noreply.github.com>
2024-04-14 20:40:36 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -102,11 +102,11 @@ DEVELOPER :samir
         let avtAnime = (await axios.get(encodeURI(
           `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
         var ok = [
-          'https://i.imgur.com/dDSh0wc.jpeg',
-          'https://i.imgur.com/UucSRWJ.jpeg',
+          'Hi https://imgur.com/gallery/DjyetdX',
+          'Hi https://imgur.com/gallery/DjyetdX',
           'https://i.imgur.com/OYzHKNE.jpeg',
-          'https://i.imgur.com/V5L9dPi.jpeg',
-          'https://i.imgur.com/M7HEAMA.jpeg'
+          'Hi https://imgur.com/gallery/DjyetdX',
+          'Hi https://imgur.com/gallery/DjyetdX'
         ]
         let background = (await axios.get(encodeURI(`${ok[Math.floor(Math.random() * ok.length)]}`), { responseType: "arraybuffer", })).data;
         fs.writeFileSync(pathAva, Buffer.from(avtAnime, "utf-8"));
